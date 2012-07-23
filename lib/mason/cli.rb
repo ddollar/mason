1bdcc903bebbe9b89838f488cdc65757d6e147af
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-07-23 11:29:03 -0400

    Fix typo.

--- lib/mason/cli.rb
+++ lib/mason/cli.rb
@@ -77,7 +77,7 @@ class Mason::CLI < Thor
         /usr/bin/env ruby -rubygems /share/mason/bin/mason build #{mason_args}
       COMMAND
 
-      FileUrils.rm_rf output
+      FileUtils.rm_rf output
       FileUtils.cp_r(File.expand_path("~/.mason/share/#{stack}/output"), output,
                      :preserve => true)
 
